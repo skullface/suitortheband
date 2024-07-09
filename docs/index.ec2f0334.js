e0f88770645cdfd67cf4e103cc0473b7f79c3801
skullface <skullface@users.noreply.github.com>
2024-07-09 08:25:23 -0600

    better hover styling :)

--- docs/index.ec2f0334.js
+++
@@ -1,2 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{document.querySelectorAll("h1, h2").forEach(e=>{let t="",n=e.textContent;e.setAttribute("aria-label",n);for(let e=0;e<n.length;e++)e%3==2?t+=`<span class='n3'>${n[e]}</span>`:e%3==1?t+=`<span class='n2'>${n[e]}</span>`:t+=n[e];e.innerHTML=t})});
-//# sourceMappingURL=index.ec2f0334.js.map
